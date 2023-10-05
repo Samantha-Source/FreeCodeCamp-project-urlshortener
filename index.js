a82b704c61bc1c0b22412e2cdbecee44849b0ac9
Samantha Brown <113550193+Samantha-Source@users.noreply.github.com>
2023-10-05 13:19:59 -0400

    Update index.js

--- index.js
+++ index.js
@@ -4,12 +4,11 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const dns = require('dns');
 const mongoose = require('mongoose');
-// const { ObjectId } = require('mongodb');
 const mongoURI = process.env.MONGO_URI;
 const app = express();
 
-// Basic Configuration
 const port = process.env.PORT || 3000;
+
 mongoose.connect(mongoURI, {useNewUrlParser: true, useUnifiedTopology: true});
 
 const urlSchema = new mongoose.Schema({
@@ -20,9 +19,11 @@ const urlSchema = new mongoose.Schema({
   short_url: String,
 })
 
-let URL = mongoose.model('URL', urlSchema);
+let URLModel = mongoose.model('URLModel', urlSchema);
 
 app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
@@ -31,9 +32,77 @@ app.get('/', function(req, res) {
 });
 
 // Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
-});
+// app.get('/api/hello', function(req, res) {
+//   res.json({ greeting: 'hello API' });
+// });
+
+async function createAndSaveURL(url) {
+  try {
+    const result = await url.save({
+      _id: url._id,
+      original_url: url.original_url,
+      short_url: url._id.toString(),
+    });
+  } catch (error) {
+    console.error('Error saving URL:', error);
+    throw new Error('Error saving URL');
+  }
+}
+
+
+app.post('/api/shorturl', async (req, res) => {
+  const { url } = req.body;
+
+  const parsedUrl = new URL(url);
+  const hostname = parsedUrl.hostname;
+
+  console.log(`parsedUrl: ${parsedUrl} //// hostname: ${hostname}`)
+  dns.lookup(hostname, (err, address, family) => {
+    if (err) {
+      console.error(err);
+      res.status(400).send({ error: 'Invalid URL' });
+      return;
+    }
+
+
+    console.log(`Address: ${address} Family: IPv${family}`);
+    
+    const newURL = new URLModel({
+      original_url: parsedUrl,
+    })
+
+    newURL.short_url = newURL._id;
+    console.log(newURL);
+    // // const result = newURL.save({
+    // //   original_url: newURL.original_url,
+    // //   short_url: newURL._id.toString(),
+    // //   _id: newURL._id,
+    // // })
+    // const result = createAndSaveURL(newURL);
+    // console.log(result)
+    try {
+      const shortUrl = createAndSaveURL(newURL);
+      res.send({ original_url: url, short_url: newURL._id })
+    } catch (error) {
+      res.status(500).send({ error: 'Error creating short URL'});
+    }
+
+
+    // res.send({ original_url: url, short_url: newURL.short_url });
+  });
+})
+
+
+
+
+
+
+
+
+
+
+
+
 
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
